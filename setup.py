53aa1ef5d1bd68f0b9322dd7049061506a943c22
joolof <joh.olofsson@gmail.com>
2020-07-08 14:06:23 -0400

    Setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="DDiT", 
+        version = "0.5",
+        author = "Johan Olofsson",
+        author_email = "johan.olofsson@uv.cl",
+        url = "https://github.com/joolof/DDiT",
+        packages = find_packages(),
+        )
