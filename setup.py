c7f1657b0b174b1a873eb0ac4168bce771f88bf6
joolof <joh.olofsson@gmail.com>
2020-07-14 14:01:11 -0400

    version 1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="DDiT", 
-        version = "0.5",
+        version = "1.0",
         author = "Johan Olofsson",
         author_email = "johan.olofsson@uv.cl",
         url = "https://github.com/joolof/DDiT",
